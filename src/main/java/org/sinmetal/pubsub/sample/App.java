f2c96ecb3b3d2ce4b80024a08671486c4118bb6a
Shingo Ishimura <metal.tie@gmail.com>
2016-11-11 18:05:57 +0900

    pull型のsubscriptionを雑に追加した

--- src/main/java/org/sinmetal/pubsub/sample/App.java
+++ src/main/java/org/sinmetal/pubsub/sample/App.java
@@ -1,7 +1,17 @@
 package org.sinmetal.pubsub.sample;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import com.google.cloud.pubsub.Message;
 import com.google.cloud.pubsub.PubSub;
 import com.google.cloud.pubsub.PubSubOptions;
+import com.google.cloud.pubsub.ReceivedMessage;
+import com.google.cloud.pubsub.Subscription;
+import com.google.cloud.pubsub.SubscriptionInfo;
 import com.google.cloud.pubsub.Topic;
 import com.google.cloud.pubsub.TopicInfo;
 
@@ -17,9 +27,37 @@ public class App {
 		// The name for the new topic
 		String topicName = "my-new-topic";
 
-		// Creates the new topic
-		Topic topic = pubsub.create(TopicInfo.of(topicName));
+		Topic topic = pubsub.getTopic(topicName);
 
-		System.out.printf("Topic %s created.%n", topic.name());
+		if (topic == null) {
+			// Creates the new topic
+			topic = pubsub.create(TopicInfo.of(topicName));
+
+			System.out.printf("Topic %s created.%n", topic.name());
+
+		}
+
+		Message message = Message.of("私がpayloadだパート2！");
+		String messageId = pubsub.publish(topicName, message);
+
+		System.out.printf("Message %s publish.%n", messageId);
+
+		String subName = "my-new-sub";
+
+		Subscription sub = pubsub.getSubscription(subName);
+		if (sub == null) {
+			sub = pubsub.create(SubscriptionInfo.of(topicName, subName));
+			
+			System.out.printf("Subscription %s created.%n", sub.name());
+		}
+
+		Consumer<ReceivedMessage> consumer = ReceivedMessage -> receiveMessage(ReceivedMessage);
+		Iterator<ReceivedMessage> messages = sub.pull(3);
+		messages.forEachRemaining(consumer);
+	}
+	
+	static void receiveMessage(ReceivedMessage msg) {
+		System.out.printf("ReceivedMessage ID %s. playload %s%n", msg.id(), msg.payloadAsString());
+		msg.ack();
 	}
 }
